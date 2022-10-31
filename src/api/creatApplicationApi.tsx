6072b0dfc4a5d38958a2e130a1ae88301cb8463b
aljurfi <malnakhli@ejada.com>
2022-10-31 09:12:04 +0300

    prettier

--- src/api/creatApplicationApi.tsx
+++ src/api/creatApplicationApi.tsx
@@ -1,33 +1,74 @@
-import React from "react"
-import type{ApplicationCreateAPIResType,ApplicationConfigration} from '../api/typs'
-import {apiCall} from './'
+import React from "react";
+import type {
+  ApplicationCreateAPIResType,
+  ApplicationConfigration,
+} from "../api/typs";
+import { ApplicationFormType } from "../containers/applicationForm/createForm";
+import { apiCall } from "./";
 
-type Props={
-  applicationInfo:ApplicationConfigration
-  
-    
-}
-export const useCreateApplicationAPI =()=>{
+type Props = {
+  applicationInfo: ApplicationConfigration;
+};
+export const useCreateApplicationAPI = () => {
+  let [apiData, setApiData] = React.useState<{
+    errors: { error: any } | null;
+    loading: boolean;
+    data: null | any;
+  }>({ errors: null, loading: true, data: null });
 
-let [apiData,setApiData]= React.useState<{errors:{error:any}|null,loading:boolean,data:null|any}>({errors:null,loading:true,data:null})
+  const submitRusame = async (applicationInfo: ApplicationFormType) => {
+    let newErros,
+      newData = null;
 
+    try {
+      let formData = new FormData();
 
+      let otherData: any = {
+        firstName: applicationInfo.firstName,
+        lastName: applicationInfo.lastName,
+        nationality: applicationInfo.nationality,
+        email: applicationInfo.email,
+        dateOfBirth: applicationInfo.dateOfBirth,
+        mobile: applicationInfo.mobile,
+        gender: applicationInfo.gender,
+        linkedIn: applicationInfo.linkedIn,
+        positionID: applicationInfo.positionID,
+      };
 
-const submitRusame =async(  applicationInfo:ApplicationConfigration)=>{
-let newErros,newData =null
+      //check if app;
+      if (applicationInfo.applicationId !== undefined) {
+        otherData.applicationId = applicationInfo.applicationId;
+      }
 
-    try{
-        const response:ApplicationCreateAPIResType = await apiCall('/createApplication','post',{data:applicationInfo})
-        if(response?.applicationId!==undefined){
-            newData =response
-        }
+      formData.append(
+        "qualifications",
+        JSON.stringify(applicationInfo.qualifications)
+      );
+      formData.append("otherData", JSON.stringify(otherData));
+      let headers: any = {};
+
+      //check form cv to upload
+      if (applicationInfo.cv !== undefined) {
+        formData.append("file", applicationInfo.cv as File);
+        headers["Content-Type"] = "multipart/form-data";
+      }
+
+      const response: ApplicationCreateAPIResType = await apiCall(
+        "/createApplication",
+        "post",
+        { data: formData, header: headers }
+      );
+      if (response?.applicationId !== undefined) {
+        newData = response;
+      }
+    } catch (e) {
+      console.log(e);
+
+      newErros = e;
     }
-    catch(e){
-        newErros=e
-    }
 
-    setApiData({loading:false,data:newData,errors:{error:newErros}})
-}
+    setApiData({ loading: false, data: newData, errors: { error: newErros } });
+  };
 
-    return [apiData.data,apiData.errors,apiData.loading,submitRusame]
-}
+  return [apiData.data, apiData.errors, apiData.loading, submitRusame];
+};
